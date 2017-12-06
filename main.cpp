504d8d8991a36076ef92cb1a531849ce0a6de0fa
Giovani Lisboa <giovanilisboa94gmail.com>
2017-12-06 01:57:19 -0300

    Finalizado

--- main.cpp
+++ main.cpp
@@ -15,78 +15,62 @@ struct impressora
 {
     char marca[20];
     int ano;
-    int area;
-    char estado[10];
     int voltagem;
+    char estado[10];
 };
 
 
 int main(int argc, char **argv)
 {
-    bool teste = false;
-    char resposta = a;
-    int item = 0;
+    cout << "STRUCT 01\n\n";
     
-    while (teste == false)
-    {
-        computador pc+item;
-        cout << "Insira os dados do computador 01. \n";
+    computador pc01;
+    cout << "Insira os dados do computador 01. \n";
     
-        cout << "Marca: ";
-        cin >> pc01.marca;
-        cout << "\n";
+    cout << "Marca: ";
+    cin >> pc01.marca;
     
-        cout << "Ano: ";
-        cin >> pc01.ano;
-        cout << "\n";
+    cout << "Ano: ";
+    cin >> pc01.ano;
     
-        cout << "Memoria RAM: ";
-        cin >> pc01.memoria;
-        cout << "\n";
+    cout << "Memoria RAM: ";
+    cin >> pc01.memoria;
     
-        cout << "Estado: ";
-        cin >> pc01.estado;
-        cout << "\n";
+    cout << "Estado: ";
+    cin >> pc01.estado;
     
-        cout << "\n";
+    cout << "\n";
     
-        cout << "Computador 01. \n";
-        cout << "Marca: " << pc01.marca << "\n";
-        cout << "Ano: " << pc01.ano << "\n";
-        cout << "Memoria RAM: " << pc01.memoria << "\n";
-        cout << "Estado: " << pc01.estado << "\n";
-
-        while (resposta != 's' || resposta != 'n')
-        {
-            cout << "Deseja inserir novo item?\n";
-            cin >> resposta;
-            cout << "\n";
-        }
-    }
+    cout << "Computador 01. \n";
+    cout << "Marca: " << pc01.marca << "\n";
+    cout << "Ano: " << pc01.ano << "\n";
+    cout << "Memoria RAM: " << pc01.memoria << " GB\n";
+    cout << "Estado: " << pc01.estado << "\n";
     
+    cout << "\n\nSTRUCT 02\n\n";
     
-    struct funcionario func[ 50 ];
-    int i = 0;
-
-    for( i = 1; i < 3; i++ )
-    {
-    	cout <<"\nNome do Funcionario " << i << ": " << '\n';
-    	cin >> func[i].nome;
-    	
-    	cout <<"Idade do Funcionario " << i << ": " << '\n';
-    	cin >> func[i].idade;
-    };
-
-    for( i = 1; i < 3; i++ )
-    {
-    	cout << "-----------------------------------\n";
-    	cout << "Nome do Funcionario " << i << ": " << func[i].nome << '\n';
-    	cout << "\nIdade do Funcionario " << i << ": " << func[i].idade << '\n';
-        cout << "-----------------------------------\n";
-        cout << "\n";
-    };
- 
-    system ("pause"); 
+    impressora imp01;
+    cout << "Insira os dados da impressora 01. \n";
+    
+    cout << "Marca: ";
+    cin >> imp01.marca;
+    
+    cout << "Ano: ";
+    cin >> imp01.ano;
+    
+    cout << "Voltagem: ";
+    cin >> imp01.voltagem;
+    
+    cout << "Estado: ";
+    cin >> imp01.estado;
+    
+    cout << "\n";
+    
+    cout << "Computador 01. \n";
+    cout << "Marca: " << imp01.marca << "\n";
+    cout << "Ano: " << imp01.ano << "\n";
+    cout << "Voltagem: " << imp01.voltagem << " V\n";
+    cout << "Estado: " << imp01.estado << "\n";
+    
     return 0;
-
 }
